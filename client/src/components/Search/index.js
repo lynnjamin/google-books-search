201632b04380a5632147ad4d39d53e0922eb53c2
Lynn Tang <tanglynn2@gmail.com>
2019-05-05 13:42:34 -0500

    search box in progess

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,23 +3,37 @@ import './styles.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-
+import axios from 'axios';
 
 function Search() {
-     const test = (e) => {
-          e.preventDefault();
-          console.log('here');
-     }
+
+    const getBooks = (e) => {
+         e.preventDefault();
+         const form = e.target.elements.searchbox.value;
+         console.log("target here: ", form);
+
+          axios.request({
+            method: 'get',
+            url: 'https://www.googleapis.com/books/v1/volumes?q=harrypotter'  
+          }).then((response) => {
+              console.log("response: ", response);
+          }).catch((error) => {
+            console.log(error);
+          });
+        }
+
      return (
           <Card body className={'cardClass'}>
                <div className="searchBox">
-                    <Form className="bookForm">
+                    <Form className="bookForm"
+                    onSubmit={e => getBooks(e)}>
+
                          <Form.Group controlId="formBasicEmail">
                               <Form.Label>Book Search</Form.Label>
-                              <Form.Control type="text" placeholder="Enter book title" />
+                              <Form.Control ref="searchBox" type="text" placeholder="Enter book title" />
                          </Form.Group>
                          <div className={'buttonArea'}>
-                              <Button className={'searchButton'} onClick={test} variant="primary" type="submit">
+                              <Button className={'searchButton'} variant="primary" type="submit">
                                    Search
                               </Button>
                          </div>
